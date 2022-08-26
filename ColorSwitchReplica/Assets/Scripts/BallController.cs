c61188e002da686a20b1eb54f137a6b09405fd9b
emiruyan <emir-uyn@hotmail.com>
2022-08-26 15:49:20 +0300

    Circle Controller and Ball Color
    CircleController script created and motion added. Our ball was given a random color by giving tags.

--- ColorSwitchReplica/Assets/Scripts/BallController.cs
+++ ColorSwitchReplica/Assets/Scripts/BallController.cs
@@ -2,17 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb; //Rigidbody 2d eriştik
     [SerializeField] private float jumpForce; //Float türünde bir jumpForce değişkeni oluşturduk
+    [SerializeField] private string currentColor;
+
+    //Renklerimize erişiyoruz;
+    [SerializeField] private Color colorCyan;
+    [SerializeField] private Color colorYellow;
+    [SerializeField] private Color colorPink;
+    [SerializeField] private Color colorPurple;
+
+    [SerializeField] private SpriteRenderer spriteRenderer; //Unity içerisindeki Sprite Renderer'a eriştik. 
+
+
+    private void Start()
+    {
+        RandomColor();
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) //Mouse sol click tıklıyor ise;
         {
             rb.velocity = Vector2.up * jumpForce; //Vector2.up ve jumpForce'u çarp ve velocity'e ata
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)//Tetiklendiği anda;
+    {
+        if (col.tag != currentColor)
+        {
+            Debug.Log("You Death"); 
+        }
+    }
+
+    void RandomColor()//Topumuza rastgele renk atayacağımız fonksiyon
+    {
+        int index = Random.Range(0, 4); //0 ve 4 arasında rastgele bir index oluşuturup index'e atadık
+        
+        switch (index) //index case'e eşit olursa ball'ın rengi değişecek
+        {
+            case 0: currentColor = "Cyan";
+                spriteRenderer.color = colorCyan; //colorCyan'ı, Ball sr color'ına atıyoruz.
+                break;
+            case 1: currentColor = "Yellow";
+                spriteRenderer.color = colorYellow;
+                break;
+            case 2: currentColor = "Pink";
+                spriteRenderer.color = colorPink;
+                break;
+            case 3: currentColor = "Purple";
+                spriteRenderer.color = colorPurple;
+                break;
         }
     }
 }
---
+++ ColorSwitchReplica/Assets/Scripts/CircleController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+ [SerializeField] private float speed; //Float tipinde bir değişken oluşturduk ve speed adını verdik
+
+ [SerializeField] private bool direction; //bool tipinde bir değişken oluşturduk ve direciton adını verdik
+
+ private void Update()
+ {
+  if (direction == false) //direction false ise;
+  {
+   transform.Rotate(0,0,speed * Time.deltaTime);
+   //x ve y 0 olarak kalsın z pozisyosnu hız ve zamanla çarpıp. Rotate edilsin
+  }
+  else
+  {
+   transform.Rotate(0,0, -1 * speed * Time.deltaTime);
+   //x ve y 0 olarak kalsın z pozisyosnu hız ve zamanla çarpıp. Rotate edilsin. -1 girmemizin nedeni tam tersine dönmesi için.
+  }
+ }
+}
